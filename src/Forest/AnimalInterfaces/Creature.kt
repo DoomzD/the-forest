86aeb6ecf9ec3864dec24c6cf724e95213b2f183
DoomzD <doomzdayforwork@gmail.com>
2019-03-28 23:57:16 +0300

    Implement creature interfaces without RPG logic.

--- src/Forest/AnimalInterfaces/Creature.kt
+++ src/Forest/AnimalInterfaces/Creature.kt
@@ -1,52 +1,69 @@
 package Forest.AnimalInterfaces
 
+import Forest.Enums.EAnimal
 import Forest.Enums.EFood
-import Forest.Enums.ETreePart
 import Forest.FieldCell
+
+import RPG.IAnimalCharacteristics
+import RPG.ISkills
+
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.random.Random
 
-interface ICreature {
+interface ICreature : IAnimalCharacteristics, ISkills {
+    val animalType: EAnimal
+
     var row: Int
     var col: Int
 
     var animalCount: Int
 
-    val food: Set<Pair<EFood, ETreePart>>
+    var stamina: Float
+    var hungriness: Float
 
-    fun makeMove(
-        field: List<List<FieldCell>>,
-        pathfinding: Int,
-        hunger: Int
-    ) {
-        if (Random.nextInt(0, 100) >= hunger)
+    val food: Set<EFood>
+
+    fun makeMove(field: List<List<FieldCell>>) {
+        if (Random.nextInt(0, 100) >= hungriness)
             return
 
         val possibleEndpoints: MutableList<Pair<Int, Int>> = mutableListOf()
         val possibleEndpointsWithFood: MutableList<Pair<Int, Int>> = mutableListOf()
 
-        (-1..1).map { dRow ->
-            (-1..1).map { dCol ->
+        (max(-1, -row)..min(1, field.size - row - 1)).map { dRow ->
+            (max(-1, -col)..min(1, field[0].size - col - 1)).map { dCol ->
                 if (field[row + dRow][col + dCol].hasFood()) possibleEndpointsWithFood += Pair(row + dRow, col + dCol)
                 else possibleEndpoints += Pair(row + dRow, col + dCol)
             }
         }
 
-        val endpoint: Pair<Int, Int> =
+        var endpoint: Pair<Int, Int> =
             if (possibleEndpoints.isEmpty() ||
-                !possibleEndpointsWithFood.isEmpty() && Random.nextInt(0, 100) < pathfinding)
+                !possibleEndpointsWithFood.isEmpty() && Random.nextInt(0, 20) < skills.pathfinding.getMultiplier()
+            )
                 possibleEndpointsWithFood.random()
             else possibleEndpoints.random()
+
+        var dist: Int = abs(endpoint.first - row) + abs(endpoint.second - col)
+        if (dist > stamina)
+            endpoint = Pair(row, col).also { dist = 0 }
+        stamina -= if (dist > 0) dist else -4
+        hungriness = min(100f, hungriness + dist)
 
         row = endpoint.first
         col = endpoint.second
     }
 
-    fun process(fieldCell: FieldCell) {
-        // TODO: Add logic
+    fun getGroupHungriness() = hungriness * animalCount
+
+    fun addFood(foodPoints: Int) {
+        hungriness -= foodPoints / animalCount
     }
 
-    private fun checkForFood(fieldCell: FieldCell): List<Pair<EFood, Int>> =
-        fieldCell.getFood().map {
-            if (it.key in food) Pair(it.key.first, it.value) else Pair(it.key.first, 0)
-        }.filter { it.second > 0 }
+    fun checkIfDead() {
+        if (hungriness == 100f)
+            animalCount -= 1
+    }
 }
--- src/Forest/AnimalInterfaces/FlyingCreature.kt
+++ src/Forest/AnimalInterfaces/FlyingCreature.kt
@@ -1,32 +1,38 @@
 package Forest.AnimalInterfaces
 
 import Forest.FieldCell
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.random.Random
 
 interface IFlyingCreature : ICreature {
-    override fun makeMove(
-        field: List<List<FieldCell>>,
-        pathfinding: Int,
-        hunger: Int
-    ) {
-        if (Random.nextInt(0, 100) >= hunger)
+    override fun makeMove(field: List<List<FieldCell>>) {
+        if (Random.nextInt(0, 100) >= hungriness)
             return
 
         val possibleEndpoints: MutableList<Pair<Int, Int>> = mutableListOf()
         val possibleEndpointsWithFood: MutableList<Pair<Int, Int>> = mutableListOf()
 
-        (-2..2).map { dRow ->
-            (-2..2).map { dCol ->
+        (max(-2, -row)..min(2, field.size - row - 1)).map { dRow ->
+            (max(-2, -col)..min(2, field[0].size - col - 1)).map { dCol ->
                 if (field[row + dRow][col + dCol].hasFood()) possibleEndpointsWithFood += Pair(row + dRow, col + dCol)
                 else possibleEndpoints += Pair(row + dRow, col + dCol)
             }
         }
 
-        val endpoint: Pair<Int, Int> =
+        var endpoint: Pair<Int, Int> =
             if (possibleEndpoints.isEmpty() ||
-                !possibleEndpointsWithFood.isEmpty() && Random.nextInt(0, 100) < pathfinding)
+                !possibleEndpointsWithFood.isEmpty() && Random.nextInt(0, 20) < skills.pathfinding.getMultiplier()
+            )
                 possibleEndpointsWithFood.random()
             else possibleEndpoints.random()
+
+        var dist: Int = abs(endpoint.first - row) + abs(endpoint.second - col)
+        if (dist > stamina)
+            endpoint = Pair(row, col).also { dist = 0 }
+        stamina -= if (dist > 0) dist else -4
+        hungriness += dist
 
         row = endpoint.first
         col = endpoint.second
