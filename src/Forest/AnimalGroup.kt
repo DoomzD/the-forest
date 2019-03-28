442e9ad28bfdd5d3c157f14231344c2753288c33
DoomzD <doomzdayforwork@gmail.com>
2019-03-28 14:17:55 +0300

    Rename AnimalsGroup -> AnimalGroup.

---
+++ src/Forest/AnimalGroup.kt
@@ -0,0 +1,6 @@
+package Forest
+import Forest.Enums.EAnimal
+
+class AnimalGroup(private val animal: EAnimal, private var animalsCount: Int) {
+
+}
--- src/Forest/AnimalsGroup.kt
+++
@@ -1,6 +0,0 @@
-package Forest
-import Forest.Enums.EAnimals
-
-class AnimalsGroup(private val animal: EAnimals, private var animalsCount: Int) {
-
-}
